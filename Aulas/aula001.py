9e4b3ec868d7c0bfeafadf94b971fd115702ca7d
Felipe Romero <felipermrcosta@gmail.com>
2024-12-06 23:01:15 -0300

    a

--- Aulas/aula001.py
+++ Aulas/aula001.py
@@ -1,1 +1,15 @@
+# print('Ola, mundo!')
+# print('Ola' + 5) = erro
+# print('Ola' , 5) = acerto 
 
+# nome='Felipe'
+# idade=18
+# peso=88.7
+
+# print(nome, idade, peso)
+
+nome=input('Qual o seu nome? ')
+peso=input('Qual o seu peso? ')
+idade=input('Quantos anos você tem? ')
+
+print('Meu nome é',nome,',eu peso',peso,'e tenho',idade,'anos')
---
+++ Exercicios/ex003.py
@@ -0,0 +1,5 @@
+numb1=input('Primeiro numero: ')
+numb2=input('Segundo numero: ')
+soma=float(numb1) + float(numb2)
+
+print('A soma é: ',soma)
