49904e7a016e6c6cf7927d329d961d12adb6651a
Felipe Romero <felipermrcosta@gmail.com>
2024-12-26 18:57:41 -0300

    a

--- Exercicios/separandonum.py
+++ Exercicios/separandonum.py
@@ -1,19 +1,26 @@
-num = str(input('Digite um numero: '))
+'''num = int((input('Digite um número: ')))
+n2 = str(10000 + num)
 
-print(f'Analisando o número {num}:')
-print(f'Unidades {num[1:2]}')
-print(f'Dezena: {num[1:4]} ')
+print(f'Analisando o número {num}')
+print(f'Unidades: {n2[4]}')
+print(f'Dezenas: {n2[3]}')
+print(f'Centenas: {n2[2]}')
+print(f'Milhar: {n2[1]}')'''
 
 
 
+#Se quer selecionar somente um caractere de uma cadeia, e so voce selecionar com []
 
+#METODO GUANABARA
 
-#  2  3
-#  0  1  2  3
+num = int(input('Informe um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
 
-# dividir = num.split()
-
-# print(dividir[0] [3])
-# print(f'Analisando o número {num}')
-# print(f'Unidade: {dividir[0] [3]}')
-# print(f'Dezena: {dividir[0] [2]}')
+print(f'Analisando o seu número {num}')
+print(f'Unidades: {u}')
+print(f'Dezenas: {d}')
+print(f'Centenas: {c}')
+print(f'Milhar: {m}')
