e4e080e526bf052c6e007bafcf3ac25c22347079
Felipe Romero <felipermrcosta@gmail.com>
2024-12-26 17:20:25 -0300

    a

--- Exercicios/separandonum.py
+++ Exercicios/separandonum.py
@@ -1,5 +1,19 @@
 num = str(input('Digite um numero: '))
-dividir = 
+
+print(f'Analisando o número {}:')
+print(f'Unidades {num[2:]}')
+print(f'Dezena: {num[1:3]}')
 
 
-print(f'Analisando o número {num}')
+
+
+
+
+
+
+# dividir = num.split()
+
+# print(dividir[0] [3])
+# print(f'Analisando o número {num}')
+# print(f'Unidade: {dividir[0] [3]}')
+# print(f'Dezena: {dividir[0] [2]}')
