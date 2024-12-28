e296b1cf00bfc4b35e5862aa5909102e8bd71403
Felipe Romero <felipermrcosta@gmail.com>
2024-12-28 20:23:36 -0300

    a

--- Exercicios/cidade.py
+++ Exercicios/cidade.py
@@ -1,4 +1,15 @@
-cid = str(input('Qual cidade você nasceu? ')).strip()
+'''cid = str(input('Qual cidade você nasceu? ')).strip()
 cid = cid.lower().split()
 
 print('santo' in cid[0])
+'''
+
+# Outro método
+
+cid = str(input('Qual cidade você nasceu? ')).strip()
+
+print(cid.lower().startswith())
+
+
+
+
