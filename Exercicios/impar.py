be35476441f8c38f889d7431f6a225d5ffc22433
Felipe Romero <felipermrcosta@gmail.com>
2025-01-01 16:50:25 -0300

    a

--- Exercicios/impar.py
+++ Exercicios/impar.py
@@ -1,6 +1,22 @@
-a = float(input('Digite um número: '))
-b = a/2
-if b == int(b):
-    print('Este número é par!')
+num = input('Digite um número aleatório: ')
+impar =  ['1', '3', '5', '7', '9']
+
+if num[-1] in impar:
+    print('É um número Impar')
 else:
-    print('Este número é ímpar!')
+    print('É um número Par')  
+
+#METODO GUSTAVO GUANABARA
+
+'''num = int(input('Digite um número aleatorio: '))
+result = str(num % 2)
+
+if result == 0:
+    print('É um número PAR')
+else:
+    print('É um número IMPAR')'''
+
+
+
+
+
