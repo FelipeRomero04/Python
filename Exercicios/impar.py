0d0ddf84a978fb711c608a2aeaf20c18c13b12b9
Felipe Romero <felipermrcosta@gmail.com>
2025-01-01 15:24:29 -0300

    a

---
+++ Exercicios/impar.py
@@ -0,0 +1,5 @@
+n = str(input('Me diga um número qualquer: '))
+l = [1, 3, 5, 7, 9]
+
+if n[-1] == [1, 3, 5, 7, 9]:
+    print('Impar')
--- Exercicios/radar.py
+++ Exercicios/radar.py
@@ -1,1 +1,8 @@
-(80 - var(km)) * 7
+km = int(input('Qual é a velocidade tua do carro? '))
+#(80 - var(km)) * 7
+
+if km > 80:
+    print(f'MULTADO! Você excedeu o limete que é de 80Km/h\nVocê deve pagar uma multa de R${(km - 80) * 7:.2f}')
+    print('PRESTE MAIS ATENÇÃO! ISSO PODE CAUSAR UM ACIDENTE!')
+else:
+    print('Tenha um bom dia! Dirija com segurança!')
