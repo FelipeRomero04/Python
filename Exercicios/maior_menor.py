a425a9929d5ddee7d500b89422397d4cca7ddf3b
Felipe Romero <felipermrcosta@gmail.com>
2025-01-05 15:15:16 -0300

    a

---
+++ Exercicios/maior_menor.py
@@ -0,0 +1,17 @@
+n1 = float(input('Primeiro valor: '))
+n2 = float(input('Segundo valor: '))
+n3 = float(input('Terceiro valor: '))
+
+if n1 > n2 > n3:
+    print(f'O maior valor digitado foi {n1}')
+    print(f'O menor valor digitado foi {n3}')
+
+if n2 > n1 > n3:
+    print(f'O maior numero é {n2}')
+    print(f'O menor numero é {n3}')
+
+if n2 > n3 > n1:
+    print(f'')
+    
+
+
