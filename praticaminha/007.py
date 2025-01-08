267ef15ebbeaf700c73a6181e7a00178f9d4e8fe
Felipe Romero <felipermrcosta@gmail.com>
2025-01-08 20:33:27 -0300

    a

--- praticaminha/007.py
+++ praticaminha/007.py
@@ -1,22 +1,20 @@
 lista = str(input('digite três números: '))
 converter = list(map(int, lista.split(',')))
 
-if min(converter) == converter[0] :    #em ordem crescente o minimo sempre vem primeiro
+if converter[0] < converter[1] < converter[2]:    
     print('Esta em ordem crescescente') 
-if max(converter) == converter[0]: #em ordem descrescente o máximo sempre vem primeiro
+elif converter[0] > converter[1] > converter[2] : 
     print('Esta em ordem descrescente')
-if max(converter) != converter[0] and min(converter) != converter[0]: 
-    print('Esta Desordenado')
+    #elif é avaliado apenas se as condições anteriores (if ou outro elif) forem falsas.
+else:
+    print('Esta desordenado')
 #Negando todas condiçoes acima
 
-''' 23234, 2321, 12424  DANDO ERRO RESOLVA'''
 
 
 
 
 
-# 1, 2, 3
-# 3, 2, 1
-# 3, 5, 1
+
 
 
---
+++ praticaminha/008.py
@@ -0,0 +1,13 @@
+idade = int(input('Digite sua idade: '))
+
+if idade < 12:
+    print('Você é uma criança')
+
+elif idade >= 12 and idade <= 17:
+    print('Você é um adolescente')
+
+elif idade >= 18 and idade <= 64:
+    print('Você é um adulto')
+
+else:
+    print('Você é um idoso')
