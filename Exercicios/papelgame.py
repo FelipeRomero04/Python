39e311aadd0a1415fa5e7b105d926bafdf0c7899
Felipe Romero <felipermrcosta@gmail.com>
2025-01-16 22:08:23 -0300

    a

--- Exercicios/papelgame.py
+++ Exercicios/papelgame.py
@@ -1,11 +1,34 @@
 from random import choice 
+from time import sleep
 
 print(''' Suas opções:
 [ 1 ] Pedra
 [ 2 ] Papel
 [ 3 ] Tesoura''')
 
-jogador = int(input('Qual é a sua jogada?'))
+jogador = int(input('Qual é a sua jogada? '))
 computador = choice(['Pedra', 'Papel', 'Tesoura'])
 
-print(computador)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+sleep(1)
+
+print('=-'*20)
+print(f'Computador jogou {computador}')
+if jogador == 1:
+    pedra = 1
+    print(f'Jogador jogou {pedra}')
+elif jogador == 2:
+    papel = 2
+    print(f'Jogador jogou {papel}')
+elif jogador == 3:
+    tesoura = 3
+    print(f'Jogador jogou {tesoura}')
+else:
+    print('Opção inválida. Tente novamente!')
+    exit()
+print('=-'*20)
+
