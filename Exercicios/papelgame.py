001593589d604184a14afa2d060ac264e9400bd1
Felipe Romero <felipermrcosta@gmail.com>
2025-01-16 23:39:35 -0300

    a

--- Exercicios/papelgame.py
+++ Exercicios/papelgame.py
@@ -7,6 +7,10 @@ print(''' Suas opções:
 [ 3 ] Tesoura''')
 
 jogador = int(input('Qual é a sua jogada? '))
+if jogador > 3:
+    print('Opção inválida. Tente novamente!')
+    exit()
+
 computador = choice(['Pedra', 'Papel', 'Tesoura'])
 
 print('JO')
@@ -19,16 +23,34 @@ sleep(1)
 print('=-'*20)
 print(f'Computador jogou {computador}')
 if jogador == 1:
-    pedra = 1
-    print(f'Jogador jogou {pedra}')
+    jogador = 'Pedra'
+    print(f'Jogador jogou {jogador}')
 elif jogador == 2:
-    papel = 2
-    print(f'Jogador jogou {papel}')
+    jogador = 'Papel'
+    print(f'Jogador jogou {jogador}')
 elif jogador == 3:
-    tesoura = 3
-    print(f'Jogador jogou {tesoura}')
-else:
-    print('Opção inválida. Tente novamente!')
-    exit()
+    jogador = 'Tesoura'
+    print(f'Jogador jogou {jogador}')
 print('=-'*20)
 
+if 'Papel' in computador and jogador == 'Pedra' or 'Pedra' in computador and jogador == 'Tesoura' or 'Tesoura' in computador and jogador == 'Papel':
+    print('Computador venceu')
+
+elif jogador == computador:
+    print('Empatou')
+
+else:
+    print('O jogador venceu')
+
+
+
+#nao precisa das vara
+
+
+    # papel ganha de pedra
+    # pedra ganha de papel
+    # 
+    #pedra ganha de tesoura
+    #tesoura ganha de papel
+
+# 'Papel' in computador and jogador == 'Pedra' or 'Pedra' in computador and jogador == 'Tesoura' or 'Tesoura' in computador and jogador == 'Papel':
