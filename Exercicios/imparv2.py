2580fa6ebb3ec91b3b31247ae121bba074d3cdfd
Felipe Romero <felipermrcosta@gmail.com>
2025-01-22 15:44:50 -0300

    a

--- Exercicios/imparv2.py
+++ Exercicios/imparv2.py
@@ -1,9 +1,29 @@
-s = 0
+'''s = 0
 
-for c in range(3, 15, 3):
+for c in range(3, 501, 3):
+    if c % 2 != 0:  # Filtrando tds os numero multiplos de 3 para somente multiplos e impares
+        s += c      # Fazendo a somatoria de todos os números
+    c = (c / 3) / 2  
+print(f'A soma de todos os {c:.0f} valores solicitados sao {s}')'''
+
+s=0
+cont = 0
+for c in range (3,501,6):
     s += c
-    print(c)
-print(s)
+    cont += 1
+print (cont, s) 
+
+        
+
+
+
+
+
+ 
+
+
+
+
 
     
 
