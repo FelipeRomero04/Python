17251c91d11dfcb16064e1ba9ce25b89b45da66f
Felipe Romero <felipermrcosta@gmail.com>
2025-01-22 00:03:15 -0300

    a

---
+++ Exercicios/imparv2.py
@@ -0,0 +1,11 @@
+s = 0
+
+for c in range(3, 15, 3):
+    s += c
+    print(c)
+print(s)
+
+    
+
+              
+    
--- Exercicios/par.py
+++ Exercicios/par.py
@@ -1,11 +1,15 @@
+for c in range(1, 51 , 2):
+    print(c, end=' ')
+
+
+
+
+
+
+# FORMA OTIMIZADA ACIMA
+
 '''for c in range(0,51):
     print('.', end='')
     if c % 2 == 0:
         print(c, end=' ')'''
 
-
-for c in range(0, 51):
-    if c % 2 == 0:
-        print(c - ' ')
-
-   
