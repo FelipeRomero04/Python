7be76c790f6fc5375f3fae001672fddfdf1d91a5
Felipe Romero <felipermrcosta@gmail.com>
2025-01-30 19:01:48 -0300

    a

--- Exercicios/analisadorv2.py
+++ Exercicios/analisadorv2.py
@@ -1,7 +1,19 @@
+somidade = 0
+velho = 0
+
+for i in range(1,3):
+    print(f'-------{i}ª Pessoa--------')
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo[F/M]: '))
+    somidade += idade
+    if velho > idade :
+        velho = idade 
+    if velho
+
+media = somidade / i
 
 
-for i in range(1,6):
-    nome 
 
 
 
