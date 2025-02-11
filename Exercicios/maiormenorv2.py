b9c82b1f80a6489cfb70a985ddce8c47a830238e
Felipe Romero <felipermrcosta@gmail.com>
2025-02-10 23:16:48 -0300

    a

--- Exercicios/maiormenorv2.py
+++ Exercicios/maiormenorv2.py
@@ -2,6 +2,42 @@ from os import system
 
 system('cls')
 
+'''lista = []
+cont = 1
+
+
 while True:
     n = int(input('Digite um número: '))
-    opcao = str(input('Quer continuar? [S/N]')).upper()
+    lista.append(n)
+    opcao = str(input('Quer continuar [S/N]? ')).upper()
+    if opcao == 'N':
+        break
+    cont += 1
+
+media = sum(lista) / len(lista)
+
+print(f'Você digitou {cont} número e a média foi {media}')
+print(f'O maior valor foi {max(lista)} e o menor foi {min(lista)}')'''
+
+opcao = 'S'
+cont = soma = maior = menor =0
+
+while opcao in 'Ss':
+    num = int(input('Digite um número: '))
+    cont += 1
+    if cont == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    
+    soma += num
+    opcao = str(input('Quer continuar [S/N]? ')).strip()[0]
+
+media = soma / cont
+
+print(f'Você digitou {cont} números é a media foi {media}.')
+print(f'O maior número é {maior} e o menor {menor}.')
+
