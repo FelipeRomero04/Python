01b014addd4398208df5904b87990ec0ea3b70e5
Felipe Romero <felipermrcosta@gmail.com>
2025-02-13 21:04:30 -0300

    a

--- Exercicios/imparv3.py
+++ Exercicios/imparv3.py
@@ -14,6 +14,44 @@ while True:
         print(f'Você não tem {jogador} dedos nas mãos. Jogue novamente!')
         jogador = int(input('Diga um valor: '))
     opcao = str(input('Par ou Ímpar[P/I]? ')).strip()[0]
+    while not opcao in 'PpIi': #Ou while opcao not in 'PpIi'
+        print('Opcão ínvalida! Tente Novamente')
+        opcao = str(input('Par ou Ímpar[P/I]? '))
+    computador = randint(9,10)
+    total = jogador + computador
+    print('-='*30)
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total}! Deu {'PAR' if total % 2 == 0 else 'IMPAR'}')
+    print('-='*30)
+    if total % 2 == 0 and opcao in 'Pp' or total % 2 == 1 and opcao in 'Ii': 
+        print('Você VENCEU!')
+        v += 1
+        print('Vamos jogar novamente...')
+        print('-='*30)
+    else:
+        print('Você PERDEU!')
+        print('-='*30)
+        break
+
+print(f'GAME OVER! Você venceu {v} {'vez' if v == 1 else 'vezes'}.')
+
+#Codigo acima esta otimizado e mais limpo, sem condições desnecessarias(os dois feitos por mim)
+
+'''from os import system 
+from random import randint
+system('cls')
+
+print('=-' * 30)
+print(' ' * 15, 'VAMOS JOGAR PAR OU ÍMPAR')
+print('=-' * 30)
+
+v = 0
+
+while True:
+    jogador = int(input('Diga um valor: '))
+    while jogador > 10:
+        print(f'Você não tem {jogador} dedos nas mãos. Jogue novamente!')
+        jogador = int(input('Diga um valor: '))
+    opcao = str(input('Par ou Ímpar[P/I]? ')).strip()[0]
     computador = randint(1,10)
     total = jogador + computador
     print('=' * 30)
@@ -34,4 +72,9 @@ while True:
         print('Você PERDEU!')
         print('=' * 30)
         break
-print(f'GAME OVER! Você venceu {v} {'vezes' if v > 1 else 'vez'}.')
+print(f'GAME OVER! Você venceu {v} {'vezes' if v > 1 else 'vez'}.')'''
+
+
+
+
+
