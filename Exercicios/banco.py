cc01d123b94800f7e5b82d318fa23337c1b63c31
Felipe Romero <felipermrcosta@gmail.com>
2025-02-16 19:49:35 -0300

    a

--- Exercicios/banco.py
+++ Exercicios/banco.py
@@ -3,47 +3,50 @@ from os import system
 system('cls')
 
 saque = float(input('Valor do saque: '))
-cedula = 200
-cont200 = cont100 = cont50 = cont20 = cont10 = cont5 = cont2 = cont1 = 0
-
-
+cedula = 50
+cont50 = cont20 = cont10 = cont1 = 0
 while True:
-    if cedula < saque:
-        saque -= cedula
-        cont200 += 1
-    elif cedula < saque:
-        cedula = 100
-        saque -= cedula
-        cont100 += 1    
-    elif cedula > saque:
-        cedula = 50
-        saque -= cedula
-        cont50 += 1
-    elif cedula > saque:
-        cedula = 20
-        saque -= cedula
-        cont20 += 1
-    elif cedula > saque:
-        cedula = 10
-        saque -= cedula
-        cont10 += 1
-    elif cedula > saque:
-        cedula = 5
-        saque -= cedula
-        cont5 += 1
-    elif cedula > saque:
-        cedula = 2
-        saque -= cedula
-        cont2 += 1
-    elif cedula > saque:
-        cedula = 1
-        saque -= cedula
-        cont1 += 1
+    while True:
+        if saque >= cedula:
+            saque -= cedula
+            cont50 += 1
+        else: 
+            break
+
+    cedula = 20
+
+    while True:
+        if saque >= cedula:
+            saque -= cedula
+            cont20 += 1
+        else:
+             break
+    
+    cedula = 10             #ARRUME UM JEITO DE SIMPLIFICAR TUDO! OBSERVE AS REPETIÇÕES NO CODIGO 
+
+    while True:
+        if saque >= cedula:
+            saque -= cedula
+            cont10 += 1
+        else:
+            break
+    
+    cedula = 1
+
+    while True:
+        if saque >= cedula:
+            saque -= cedula
+            cont1 += 1
+        else:
+            break
+
+    
     if saque == 0:
         break
 
-print(cont200)
 
+
+print(cont50, cont20, cont10, cont1)
 
 
 #resetar sempre q a cedula muda e armazenar o valor
