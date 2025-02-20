e6b5948a21cd6973eca27f18f32d05a2b7901d0c
Felipe Romero <felipermrcosta@gmail.com>
2025-02-19 23:46:07 -0300

    a

--- Exercicios/banco copy.py
+++ Exercicios/banco copy.py
@@ -2,10 +2,14 @@ from os import system
 
 system('cls')
 
+'''print('='*30)
+print(f'{'Banco CEV':^30}')
+print('='*30)
+
 saque = float(input('Valor do saque: '))
 
 cedula = [50, 20, 10, 1]
-indice = 0   #Variavel para ser usada com indice
+indice = 0   #Variavel para ser usada com indice(ESTRATÉGIA INTERESSANTE!!!!!! COPIAR)
 cont = [0, 0, 0, 0]
 
 while saque != 0:
@@ -16,42 +20,15 @@ while saque != 0:
     else:
         cedula.remove(cedula[0]) 
         indice += 1 #Atualizando o indice para andar pra frente, conforme as cedula mudam
-           
-for c, qnts in zip(cedula, cont): #Para percorrer duas listas ao mesmo tempo zip()
+
+cedula = [50, 20, 10, 1]
+
+for notas, c in zip(cedula, cont): #Para percorrer duas listas ao mesmo tempo zip()
+    if c > 0:
+        print(f'Total de {c} cédulas de R${notas}') 
+print('==' * 20)
+print('Volte Sempre! Ao banco CEV')'''
     
-    print(f'{c}')
-    
-    
-        
-#VAI NO CHATGPT E LEIA A EXPLICAÇÂO NO ULTIMO CHAT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-    #print(f'Total de {cont[0]} cedula de R$50')
-
-  
-
-
-
-
-
-
-            
-            
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-        
 
 
 
@@ -98,49 +75,3 @@ while True:
         break
 '''
 
-
-
-    
-    
-
-
-
-
-# Cada vez que o saque nao puder ser reduzido pela maior nota, a cedula atualiza para uma menor. Se
-
-    
-'''
-if cedula < saque:
-    cedula = 20
-elif cedula < saque:
-    cedula = 10
-
-
-'''
-
-
-
-'''while saque != 0:
-    if saque >= cedula:
-        saque -= cedula
-        cont50 += 1
-    elif saque < cedula:
-        cedula = 20
-        saque -= cedula
-        cont20 += 1
-    elif saque < cedula:
-        cedula = 10
-        saque -= cedula
-        cont10 += 1
-    else:
-        cedula = 1
-        saque -= cedula
-        cont1 += 1'''
-
-
-
-
-
-
-
-#resetar sempre q a cedula muda e armazenar o valor
