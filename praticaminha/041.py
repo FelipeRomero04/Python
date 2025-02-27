6f722e039fd99d4625ecb517cdd10258321beeda
Felipe Romero <felipermrcosta@gmail.com>
2025-02-27 00:33:19 -0300

    a

--- praticaminha/041.py
+++ praticaminha/041.py
@@ -1,9 +1,17 @@
+from os import system
+
+system('cls')
+
 f1 = 0
 f2 = 1
-total = ''
+total = 0
 
 for i in range(0,15):
-    total += f1
-    print(f'{total},', end='')
-    f1 += f2
-    f2 = total
+    total = f1
+    if i == 14:
+        print(f'{total} ', end='')
+    else:
+        print(f'{total}, ', end='')
+
+    f1 = f2
+    f2 = total + f1
---
+++ praticaminha/042.py
@@ -0,0 +1,31 @@
+matriz = []
+valor = 0
+
+
+'''for l in range(1,6):
+    matriz.append(l)
+print(matriz)
+'''
+
+'''for l in range(0,3):
+    lista = []
+    for c in range(3):
+        lista.append(valor)
+        valor += 1
+    matriz.append(lista)
+for linha in matriz:
+    print(lista)'''
+
+for l in range(0,3):
+    lista = []
+    for c in range(3):
+        lista.append(valor)
+        valor += 1
+    matriz.append(lista)
+for m in matriz:
+    print(m)
+   
+
+   
+   
+
