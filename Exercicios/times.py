bea664f968eb103c1ed84ba6cd1d09e9569f7b45
Felipe Romero <felipermrcosta@gmail.com>
2025-03-01 18:39:57 -0300

    a

--- Exercicios/times.py
+++ Exercicios/times.py
@@ -1,5 +1,5 @@
 from os import system
-
+import pandas as pd
 system('cls')
 
 
@@ -13,7 +13,6 @@ times_brasileirao_2024 = (
 
 time = str(input('Escreva o nome do seu time: '))
 
-
 print(f'Lista de times do Brasileirão: {times_brasileirao_2024}')
 print('=-' * 66)
 print(f'Os 5 primeiros são: {times_brasileirao_2024[:5]}')
@@ -23,6 +22,22 @@ print('=-' * 66)
 print(f'Times em ordem alfabética: {sorted(times_brasileirao_2024)}')
 
 cont = 0
+
+'''for t in times_brasileirao_2024:
+    cont += 1
+    if t == time:
+        print(f'O {time} está na {cont}ª posicão')'''
+
+#OUUUU(ABAIXO)      
+
+for t in range(len(times_brasileirao_2024)):
+    if times_brasileirao_2024[t] == time:
+        print(f'O {time} está na {t + 1}ª posição')
+
+
+
+
+
 
         
        
