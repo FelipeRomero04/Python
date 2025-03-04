bfd5914b0f92e7a34c0d2e6ce91ff6779824e299
Felipe Romero <felipermrcosta@gmail.com>
2025-03-04 14:26:58 -0300

    a

--- Exercicios/tuplapreco.py
+++ Exercicios/tuplapreco.py
@@ -3,13 +3,13 @@ lista = (
    'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99,
    'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
 
-print('--' * 20)
+'''print('--' * 20)
 print(f'{'LISTAGEM DE PREÇOS':^38}')
 print('--' * 20)
 
 for p, v in zip(range(0,len(lista), 2), range(1,len(lista), 2)):
-   print(f'{lista[p]:.<25} {lista[v]:.2f}')
-print('--' * 20)
+   print(f'{lista[p]:.<25} R${lista[v]:.2f}')
+print('--' * 20)'''
 
 #SEMPRE QUE TIVER UMA COLECÃO DE ELEMENTOS COM VALORES INTERCALADOS, USE A ESTRATEIA DO PAR OU IMPAR / TIPAGEM / RANGE DE 2 EM 2.
 
@@ -17,4 +17,10 @@ print('--' * 20)
 
 #Implementar a opcao de voce mesmo colar mais itens
 
+#Metodo guanabara
 
+for p in range(len(lista)):
+   if p % 2 == 0:
+      print({lista[p]})
+   else:
+      print(lista[p])
