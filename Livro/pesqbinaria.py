b7eb6090482f8a0672471d3e0c7af55205899501
Felipe Romero <felipermrcosta@gmail.com>
2025-03-05 14:49:07 -0300

    a

---
+++ Livro/pesqbinaria.py
@@ -0,0 +1,16 @@
+lista = [1, 3, 5, 7, 9]
+
+def pesquisa_binaria(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+    while baixo <= alto:
+        meio = (baixo + alto) / 2
+        chute = lista[meio]
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+        return none
+print(pesquisa_binaria(lista, 3))
