b72faeeb26ab848652cd570dc9f85d543f7a5c80
Felipe Romero <felipermrcosta@gmail.com>
2025-03-06 15:42:02 -0300

    a

--- Exercicios/insert.py
+++ Exercicios/insert.py
@@ -1,17 +1,32 @@
 lista = []
 
-
-
-for v in range(5):
+for v in range(5 + 1):
     n = int(input('Digite um valor: '))
-    lista.append(n)
-    maior = menor = lista[0]
-    if maior > n:
-        maior = n
-    if menor < n:
-        menor = n
+    
+    if v == 0 or n > max(lista):
+        lista.append(n)
+        print('Adicionado ao fim da lista...')
+    if lista[0] < n:
+        lista.insert(v, n)
+        print('Adicionado posição zero')
+        
+            
 
-    for i in lista:
+
+   
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    '''for i in lista:
         if maior >= n or v == 0:
             print(f'Adicionado ao final da lista...')
         elif maior < n > menor:
@@ -19,6 +34,10 @@ for v in range(5):
             print('Adicionado na posição 1 da lista...')
         elif n <= menor:
             lista.insert(0, n)
-            print(f'Adicionado na posição 0 da lista...')
+            print(f'Adicionado na posição 0 da lista...')'''
         
-        
+'''
+1. Usar append para colocar o item no final da lista
+
+2. Usar insert para colocar na posição 0 ou 1, para que as lista se ordene sozinha
+'''      
--- testes/testes.py
+++ testes/testes.py
@@ -1,9 +1,4 @@
-lista = []
-for c in range(0, 5):
-    num = int(input(f'Digite um número para a posição {c+1}: '))
-    lista.append(num)
-nummax = max(lista)
-nummin = min(lista)
-print(f'Os valores digitados foram: {lista}')
-print(f'O maior valor digitado foi: {max(lista)} e a sua posição é: {lista.index(nummax)+1}')
-print(f'O menor valor digitado foi: {min(lista)} e a sua posição é: {lista.index(nummin)+1}')
+lista = [3, 5, 6, 1, 3]
+var = 9
+lista.insert(0, var)
+print(lista)
