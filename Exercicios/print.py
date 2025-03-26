41078320a98bc04ec72ec8bcf6639b6715bf8c1a
Felipe Romero <felipermrcosta@gmail.com>
2025-03-26 00:01:03 -0300

    a

---
+++ Exercicios/print.py
@@ -0,0 +1,9 @@
+def escreva(txt):
+    print(f'{'~' * len(txt):^2}')
+    print(f'{txt:5}')
+    print(f'{'~' * len(txt)}')
+
+
+
+
+escreva('cvc')
--- Exercicios/terreno.py
+++ Exercicios/terreno.py
@@ -1,8 +1,19 @@
+def area(l, c):
+    a_total = l * c
+    print(f'A área de um terreno {l:.1f}x{c:.1f} é de {a_total}m².')
+
 print('Controle de Terreno')
 print('-' * 30)
-
-def area(l, c):
-    total = l * c 
-    print(f'A área de um terreno {l:.1f}x{c:.1f} é de {total}m².')
-
 area(float(input('LARGURA (m): ')), float(input('COMPRIMENTO (m): ')))
+
+#Outro metodo
+
+def area(larg, comp):
+    a_total = larg * comp
+    print(f'A área de um terreno {larg}x{comp} é de {a_total}m².')
+
+print('Controle de terreno.')
+print('-' * 30)
+l = float(input('LARGURA(m): '))
+c = float(input('COMPRIMENTO(m): '))
+area(l, c)
