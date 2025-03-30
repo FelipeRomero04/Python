fd4cd331fb3fd7085a73fa0e81a384951ef5f3a9
Felipe Romero <felipermrcosta@gmail.com>
2025-03-29 23:06:00 -0300

    a

--- praticaminha/projetos/todolist.py
+++ praticaminha/projetos/todolist.py
@@ -1,10 +1,26 @@
 def ad_tarefa(lista):
-    if opcao == '1':
-        name_task = input('Adicione uma tarefa: ')
-        if name_task in lista:
-            print('Essa tarefa já esta na lista')
+    
+    name_task = input('Adicione uma tarefa: ')
+    if name_task in lista:
+        print('Essa tarefa já esta na lista')
     tasks.append(name_task)
+    return name_task
 
+
+
+def task_end():
+    
+    show_tasks(tasks)
+    escolha = input('Número da tarefa concluida: ')
+    return escolha
+
+def show_tasks(lista):
+    
+    for i, v in enumerate(lista, start=1):
+        if i == task_end():
+            print(f'{i}. [V] {v}')
+        else:
+            print(f'{i}. [ ] {v}')
 
 tasks = []
 
@@ -13,17 +29,19 @@ while True:
 
     print('''
     1. Adicionar Tarefas 
-    2. Listar Tarefas
-    3. Remover Tarefas
-    4. Concluir Tarefas
+    2. Concluir Tarefas
+    3. Listar Tarefas
+    4. Remover Tarefas
     ''')
 
     opcao = input('Selecione uma das opções acima[ENTER p/sair]: ')
     if not opcao:
-        exit()
+        break
+    opcao = int(opcao)
+    if opcao == 1:
+        ad_tarefa(tasks)
+    if opcao == 2:
+        task_end()
+    if opcao == 3:
+        show_tasks(tasks)
 
-    
-
-    ad_tarefa(tasks)
-
-    print(tasks)
