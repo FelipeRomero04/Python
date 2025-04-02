e7b494ce237e16a2b5bbc5ea82377625d83832b0
Felipe Romero <felipermrcosta@gmail.com>
2025-04-01 23:36:27 -0300

    a

--- praticaminha/projetos/todolist.py
+++ praticaminha/projetos/todolist.py
@@ -1,12 +1,23 @@
+def menu():
+    print('=== Gerenciador de Tarefas ===')
+    print('''
+    1. Adicionar Tarefas
+    2. Concluir Tarefas
+    3. Listar Tarefas
+    4. Remover Tarefas
+    5. Sair
+    ''')
+
 def add_task():
+    print('=-' * 30)
     new_task = input('Digite uma tarefa: ').strip().capitalize()
     if new_task in tasks:
         print('Tarefa ja incluida!')
         return
     tasks.append(new_task)
+    print('Tarefa adiciona a lista.')
 
 def end_task(lista): #concluida
-
     show_tasks(tasks, num_tasks_end)    
     while True:
         completed = int(input('Nº da tarefa à concluir: '))
@@ -24,17 +35,18 @@ def end_task(lista): #concluida
         break
 
 def show_tasks(lista1, lista2):
+    print('~' * 30)
     print('Mostrando a lista: ')
     max_len = max(len(lista1), len(lista2))
-
     for i in range(max_len):
         if i in lista2:
             print(f'{i}. [v]{lista1[i]}')
             continue
         print(f'{i}. [ ]{lista1[i]}')    
-
     if not lista1:
         print('Sua Lista de Tarefas está vazia!')
+    print('~' * 30)
+    menu()
 
 def remove_task(lista):
     show_tasks(tasks, num_tasks_end)
@@ -58,18 +70,11 @@ tasks = []
 num_tasks_end = []
 
 
-print('Gerenciador de Tarefas')
-print('''1. Adicionar Tarefas
-2. Concluir Tarefas
-3. Listar Tarefas
-4. Remover Tarefas
-''')
+menu()
 
 while True:
-
-    opcao = input('Selecione uma das opções acima[ENTER p/sair]: ')
-    if not opcao:
-        break
+    print('=-' * 30)
+    opcao = input('Selecione uma das opções acima: ')
     opcao = int(opcao)
     
     if opcao == 1:
@@ -80,6 +85,11 @@ while True:
         show_tasks(tasks, num_tasks_end)    
     if opcao == 4:
         remove_task(tasks)
+    if opcao == 5:
+        opcao = input('ENTER P/SAIR: ')
+        if not opcao:
+            break
+        continue
 
 #Tratar ValueError e finalizações(deixa bunitin)
 
