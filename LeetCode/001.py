8c1aac8dc38a2d93abfa179024dc8b958dcaae36
Felipe Romero <felipermrcosta@gmail.com>
2025-04-19 18:13:12 -0300

    a

--- LeetCode/001.py
+++ LeetCode/001.py
@@ -1,16 +1,23 @@
-# num = [2,5,7,3]
-# target = 12
-# ind_som = []
-# for i in range(len(num)):
-#     for ind, m in enumerate(num):
-#         soma = num[i] + m
-#         if soma == target:
-#             ind_som.append(i)
-#             ind_som.append(ind)
-            
+EXPECTED_BAKE_TIME = 40
 
-# print(ind_som)
+def bake_time_remaining(minutos):
+    '''Recebe o parâmetro 'minutos', que sera usada como meio de retornar, quantos ainda faltam para lasanha ficar pronta'''
+    if minutos < EXPECTED_BAKE_TIME:
+        return EXPECTED_BAKE_TIME - minutos
+    raise ValueError('Sua Lasanha passou do ponto.')
+    # return 'Sua lasanha passou do ponto!'
 
-dic = {1:0}
+def preparation_time_in_minutes(number_of_layers):
+    '''Calcula quantos minutos adicionais a lasanha terá que ficar no forno, de acordo com a quantidade de camadas/ cada camada +2 min'''
+    return 2 * number_of_layers
+    
 
-print(dic)
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    '''Calcula o tempo total de cozimento da lasanha''' 
+    return elapsed_bake_time + preparation_time_in_minutes(number_of_layers)
+
+print(EXPECTED_BAKE_TIME)
+print(bake_time_remaining(30))
+print(preparation_time_in_minutes(2))
+print(elapsed_time_in_minutes(3, 20))
+
