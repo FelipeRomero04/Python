e433674d88d60448352fd2111080dfe49b73b8ab
Felipe Romero <felipermrcosta@gmail.com>
2025-04-29 00:53:28 -0300

    a

--- testes/hacking.py
+++ testes/hacking.py
@@ -29,16 +29,42 @@ def value_of_card(card):
 
 
 def higher_card(card_one, card_two):
+   
     
-    if card_one in ('J', 'Q', 'K'):
-        card_one = 10
 
-    return (card_one, card_two) if card_one == card_two else max(card_one, card_two)
-
-print(higher_card('K', '10'))
-
+        
+            
+    
+    return (card_one, card_two) if value_of_card(card_one) == value_of_card(card_two) else card_one if value_of_card(card_one) > value_of_card(card_two) else card_two
 
 
+
+def value_of_ace(card_one, card_two):
+    if card_one == 'A':
+        card_one = 11
+        
+    
+    elif card_two == 'A':
+        card_two = 11
+        
+    return 1 if value_of_card(card_one) + value_of_card(card_two) > 10 else 11
+
+'''Os cards assumindo o valor de 11 na função não altera nada. O 11 sera passado com parametro para value_of_card, a função value_of_card retorna o valor do card como int. Como o 11 ja é um int ele sera apenas retornado novamente'''
+
+
+def is_blackjack(card_one, card_two):
+    ace = False
+
+    if card_one == 'A':
+        ace = True
+    
+    elif card_two == 'A':
+        ace = True
+    
+
+    return True if ace and card_one in ('10', 'J', 'Q', 'K') or card_two in ('10','J', 'Q', 'K') else False
+
+print(is_blackjack('Q', 'K'))
 
 
 
