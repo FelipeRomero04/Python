45b2d9b018611748c86fccaa346163acd15c1a07
Felipe Romero <felipermrcosta@gmail.com>
2025-05-03 18:50:20 -0300

    a

--- LeetCode/007.py
+++ LeetCode/007.py
@@ -1,3 +1,5 @@
+from math import floor
+
 def get_rounds(number):
     return list(i + number for i in range(3))
 
@@ -13,12 +15,53 @@ def concatenate_rounds(rounds_1, rounds_2):
     return rounds_1 + rounds_2
 
 def approx_average_is_average(hand):
-    mediana = len(hand) / 2
-    media = sum(hand) / len(hand)
-    return (hand[0] + hand[-1] / 2) or mediana == media
 
-print(approx_average_is_average([1, 2, 3]))
+    # O first_last(media da primeira e ultima carta) ou a carta no meio do baralho(index_mediana) tem q ser == media de hand
+
+    media = sum(hand) / len(hand)
+    first_last = (hand[0] + hand[-1]) // 2
+    mediana = hand[len(hand) // 2] 
+
+    return media in (first_last, mediana)
+
+    #VOLTAR NESSA TÁ MUITO RUIM
+
+def average_even_is_average_odd(hand):
+    """Return if the (average of even indexed card values) == (average of odd indexed card values).
+
+    :param hand: list - cards in hand.
+    :return: bool - are even and odd averages equal?
+    """
+    num_impar = []
+
+    for i in range(1, len(hand), 2):
+        num_impar.append(hand[i]) 
+        
+    media_impar = sum(num_impar) / len(num_impar)
+
+    num_par = []
+
+    for i in range(0, len(hand), 2):
+        num_par.append(hand[i])
+
+    media_par = sum(num_par) / len(num_par)
+
+    return media_impar == media_par
+
+
+
 
 #Copiar sobre desempacotamento
 #Copiar a diferença de '**' e '*' na passagem de parâmetros
 #Voltar ao exercício anterior e ler o código da comunidade
+def maybe_double_last(hand):
+    
+    if hand[-1] == 11:
+        hand[-1] = hand[-1] * 2
+
+    return hand
+
+print(maybe_double_last([5, 9, 10]))
+
+    
+    
--- testes/hacking.py
+++ testes/hacking.py
@@ -5,5 +5,22 @@ def apresentar(nome, idade):
 
 dados = {'nome': 'Fernando', 'idade': 21}
 
-apresentar(**dados)
+
 # Equivale a: apresentar(nome='Fernando', idade=21)
+
+
+lista = [1, 2, 4, 2]
+item1, item2 = 4 , 1
+
+for x in (item1, item2):
+    print(x)
+
+# for i in range(len(lista), 2):
+#     print(i)
+
+    #  for i in range(0, len(hand), 2):
+    #     num_par.append(hand[i])
+
+    # media_par = sum(num_par) / len(num_par)
+
+
