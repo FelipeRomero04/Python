7ffea9461648b096dc0f02710b38b449babe9ef1
Felipe Romero <felipermrcosta@gmail.com>
2025-05-03 00:18:25 -0300

    a

---
+++ LeetCode/007.py
@@ -0,0 +1,13 @@
+def get_rounds(number):
+    return list(i + number for i in range(3))
+
+
+
+def concatenate_rounds(rounds_1, rounds_2):
+    """Concatenate two lists of round numbers.
+
+    :param rounds_1: list - first rounds played.
+    :param rounds_2: list - second set of rounds played.
+    :return: list - all rounds played.
+    """
+    return rounds_1 + rounds_2
--- testes/hacking.py
+++ testes/hacking.py
@@ -1,21 +1,9 @@
-def capitalize_title(title):
-    """Convert the first letter of each word in the title to uppercase if needed.
-
-    :param title: str - title string that needs title casing.
-    :return: str - title string in title case (first letters capitalized).
-    """
-
-    return title.title() 
 
 
-def replace_word_choice(sentence, old_word, new_word):
-    """Replace a word in the provided sentence with a new one.
+def apresentar(nome, idade):
+    print(f"Meu nome é {nome} e tenho {idade} anos.")
 
-    :param sentence: str - a sentence to replace words in.
-    :param old_word: str - word to replace.
-    :param new_word: str - replacement word.
-    :return: str - input sentence with new words in place of old words.
-    """
-    
-    return sentence.replace(old_word, new_word)
+dados = {'nome': 'Fernando', 'idade': 21}
 
+apresentar(**dados)
+# Equivale a: apresentar(nome='Fernando', idade=21)
