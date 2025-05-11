07504b68258ca60fab02cb6e70d24c90ab4677cf
Felipe Romero <felipermrcosta@gmail.com>
2025-05-11 16:43:13 -0300

    a

--- LeetCode/009.py
+++ LeetCode/009.py
@@ -35,4 +35,5 @@ def clean_up(combined_record_group):
         print(i)
     
 
+
 print(clean_up((('Brass Spyglass', '4B', 'Abandoned Lighthouse', ('4', 'B'), 'Blue'), ('Vintage Pirate Hat', '7E', 'Quiet Inlet (Island of Mystery)', ('7', 'E'), 'Orange'), ('Crystal Crab', '6A', 'Old Schooner', ('6', 'A'), 'Purple'))))
