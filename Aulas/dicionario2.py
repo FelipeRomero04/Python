6269f27c45eed7c66cfa9e63891bdff2ab9db7b0
Felipe Romero <felipermrcosta@gmail.com>
2025-05-14 22:10:21 -0300

    a

--- Aulas/dicionario2.py
+++ Aulas/dicionario2.py
@@ -1,9 +1,44 @@
 
 dados = {'nome': 'Felipe', 'Idade': 18}
 
-dados2 = dados.setdefault('sobrenome')
-#Retorna o valor se a chave for encontrada
-#Caso não for encontrada retorna o valor
+dados2 = dados.setdefault('nome', 'Romero')
+#Retorna o valor se a chave for encontrada!
 
-print(dados)
+#Caso não for encontrada, irá retornar o valor colocado em <default value> e inserido no dict. Caso o <default value> não seja definido, somente a key, o valor None será inserido junto a key ao dicionário
 
+
+# print(dados2)
+
+
+#
+
+
+new_dict.fromkeys(['branco', 'preto', 'marrom', 'azul', 'roxo'], 'sexo')
+# print(new_dict)
+
+#Forma de criar um dicionario de rapidamente usando um iteravel e um valor fixo
+
+#
+
+dados1 = {'nome' : 'José', 'idade': 100, 'sexo': 'Trans'}
+dados2 = {'email': 'joseperera@teste.com', 'endereco': 'Debaixo da ponte'}
+
+dados_completos = dados1 | dados2
+
+#print(dados_completos)
+
+#Union - Cria um novo dict concatenando dois dicionarios 
+
+#
+ 
+set1 = {1, 2, 3, 4, 5, 6}
+set2 = {7, 8, 9}
+
+set1 |= set2
+
+print(set1)
+
+
+#Operador de Union com atribuição. Não tem necessidade de criar um novo dict. Tem as mesmas regras que o operador de Union normal(chaves repetidas, o dict a ser unido(o da direta da |) o valor precede o valor do dict original)
+
+#Ao fazer a união com atribuição o dict principal(a esquerda) e tem o valor substituido pelo valor concatenado dos dois dict)
---
+++ LeetCode/011.py
@@ -0,0 +1,23 @@
+def add_item(current_cart, items_to_add):
+    """Add items to shopping cart.
+
+    :param current_cart: dict - the current shopping cart.
+    :param items_to_add: iterable - items to add to the cart.
+    :return: dict - the updated user cart dictionary.
+    """
+
+
+
+
+    # return {current_cart.setdefault(items_to_add[i], 1) if items_to_add[i] not in current_cart else k: v + items_to_add.count(k) for i, (k, v) in enumerate(current_cart.items())}
+
+    
+
+    return {k: v + items_to_add.count(k) if items_to_add[i] in current_cart else current_cart.setdefault(items_to_add[i], 1) for i, (k ,v) in enumerate(current_cart.items())} #Ver no chatgpt depois de fazer da forma normal se e possivel
+    # return {k: v + items_to_add.count(k) for k, v in current_cart.items()}
+
+print(add_item({'Banana': 3, 'Apple': 2, 'Orange': 1},
+              ('Apple', 'Apple', 'Orange', 'Apple', 'Banana', 'Pessego')))
+
+
+    
