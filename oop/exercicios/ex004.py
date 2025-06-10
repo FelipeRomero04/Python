cfddcdbf6739fff59f068dcbc5daa881ec7d5658
Felipe Romero <felipermrcosta@gmail.com>
2025-06-10 20:39:19 -0300

    a

--- oop/exercicios/ex004.py
+++ oop/exercicios/ex004.py
@@ -1,16 +1,22 @@
 class Aluno:
     def __init__(self, nome, *notas):
         self.nome = nome
-        self.notas = notas
+        self.notas = list(notas)
+        
 
-    def adicionar_notas(self, *notas):
-        self.notas.append(notas)
+    def adicionar_notas(self, nota):
+        list_nota = []
+        for n in self.notas:
+            list_nota.append(n)
+        list_nota.append(nota)
+
+        self.notas.append(nota)
         return f'As notas do aluno {self.nome} são {self.notas}'
 
 
-dados_aluno = Aluno('Felipe', [8, 5, 2])
+dados_aluno = Aluno('Felipe', (4,2,5,1))
 
-print(dados_aluno.adicionar_notas([10, 4, 2]))
+print(dados_aluno.adicionar_notas(4))
 
         
 #Ver como colocar listas nisso esqueci
