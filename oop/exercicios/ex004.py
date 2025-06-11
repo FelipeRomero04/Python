5f10225b58e0ff622112898e46adf05fa58f0b67
Felipe Romero <felipermrcosta@gmail.com>
2025-06-11 20:41:20 -0300

    a

--- oop/exercicios/ex004.py
+++ oop/exercicios/ex004.py
@@ -2,35 +2,58 @@ class Aluno:
     def __init__(self, nome):
         self.nome = nome
         self.notas = []
-       
-    def adicionar_notas(self, *nota):
-        try:
-            for n in nota:
-                if not 0 <= n <= 10:
-                    return 'ERRO: As notas devem ser definidas de 0 á 10.'
-                self.notas.append(n)
-
-        except Exception:
-            raise ('Erro: Os valores informados são inválidos...') 
 
 
-    def media(self):
-        media = float(sum(self.notas) / len(self.notas))
-        return media
-    
-    def mostrar_notas(self):
-        return f'Aluno: {self.nome} \nNotas: {self.notas} \nMédia: {self.media()}'
+    def adicionar_notas(self, *notas):
         
+        invalid_grade = 0
+        
+        for i, n in enumerate(notas):
+            
+            if 0 <= n <= 10:
+                self.notas.append(n)
+                continue
+    
+            invalid_grade += 1
+
+        if invalid_grade > 0:
+            print(f'Notas inválidas removidas: {invalid_grade}')
+
+      
+    def media(self):
+        if not self.notas:
+            return None
+
+        try:
+            media = sum(self.notas) / len(self.notas)
+            return media
+        except ZeroDivisionError:
+            return 'Houve um erro na validação das notas'
+    
+
+    def mostrar_notas(self):
+        media = self.media()
+        if media is None:
+            return f'Aluno: {self.nome}\nNotas: (Sem notas disponiveis)\nMédia: 0'
+        return 
+
 
 dados_aluno = Aluno('Felipe')
-
-print(dados_aluno.adicionar_notas(10, -1, 4, 2))
-print(dados_aluno.mostrar_notas())
-
-
-#Arruma isso putaqpariu
-        
-#Ver como colocar listas nisso esqueci
+try:
+    dados_aluno.adicionar_notas()
+    print(dados_aluno.mostrar_notas())
+except TypeError:
+    print ('ERROR: Apenas digitos são permitidos no campo.')
 
 
-#Por que não consigo mudar a tupla pra list?
+
+
+
+
+# def adicionar_notas(self, *notas):
+#         self.notas = list(n if 0 <= n <= 10 else -1 for n in notas )
+#         if -1 in self.notas:
+#             self.notas = 'Notas inválidas! Tente novamente.'
+
+
+
