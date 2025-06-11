c96bb324cf35e3fde686d262a6fdec751036fc85
Felipe Romero <felipermrcosta@gmail.com>
2025-06-10 23:10:25 -0300

    a

--- oop/exercicios/ex004.py
+++ oop/exercicios/ex004.py
@@ -1,19 +1,29 @@
 class Aluno:
-    def __init__(self, nome, *notas):
+    def __init__(self, nome, notas):
         self.nome = nome
         self.notas = notas
+       
+    def adicionar_notas(self, *nota):
+        try:
+            for n in nota:
+                self.notas.append(n)
+            return f'As notas do aluno {self.nome} são {self.notas}'
+        except Exception:
+            raise ('Erro: Os valores informados são inválidos...') 
+
+
+    def media(self):
+        media = float(sum(self.notas) / len(self.notas))
+        return f'Média do aluno: {self.nome} - {media:.2f}'
         
 
-    def adicionar_notas(self, nota):
-        
-        list_nota = list(self.notas)
-        list_nota.append(nota)
-        return f'As notas do aluno {self.nome} são {self.notas}'
+dados_aluno = Aluno('Felipe', [4,2,5,1])
 
-
-dados_aluno = Aluno('Felipe', (4,2,5,1))
-
-print(dados_aluno.adicionar_notas(4))
+print(dados_aluno.adicionar_notas(4,4,2,1))
+print(dados_aluno.media())
 
         
 #Ver como colocar listas nisso esqueci
+
+
+#Por que não consigo mudar a tupla pra list?
